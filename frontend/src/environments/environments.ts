dbb3479188f7044d475206dc0e00e6239e937565
Nano <voutsina.alejandro@gmail.com>
2026-06-23 10:54:28 -0300

    backend vercel fix

--- frontend/src/environments/environments.ts
+++ frontend/src/environments/environments.ts
@@ -1,6 +1,6 @@
 export const environments = {
   url: 'https://agyvrqfvvwacjvgvmaxg.supabase.co',
   key: 'sb_publishable_uN7KhBrylb2VB1BVCkGWGA_yoKwE_Es',
-  apiUrl: 'http://localhost:3000',
+  apiUrl: 'https://alejandro-voutsina-tp-2-prog-4-2026.vercel.app/_/backend',
   bucket: 'imagenes',
 };
