049c1dae283d6bce9d9170c01ae3f43957008bff
Nano <voutsina.alejandro@gmail.com>
2026-06-30 14:29:33 -0300

    Actualziacion apiURL deploy

--- frontend/src/environments/environments.ts
+++ frontend/src/environments/environments.ts
@@ -1,6 +1,6 @@
 export const environments = {
   url: 'https://agyvrqfvvwacjvgvmaxg.supabase.co',
   key: 'sb_publishable_uN7KhBrylb2VB1BVCkGWGA_yoKwE_Es',
-  apiUrl: 'http://localhost:3000',
+  apiUrl: 'https://alejandro-voutsina-tp2-prog4-2026-c1-79a1.onrender.com',
   bucket: 'imagenes',
 };
